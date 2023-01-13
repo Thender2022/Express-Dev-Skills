f8bfc9e1ad9cd74c47c2bf73d441fde3eb1d491b
Thender2022 <Thender2022@aol.com>
2023-01-12 19:37:29 -0800

    lab update

--- controllers/investments.js
+++ controllers/investments.js
@@ -5,17 +5,17 @@ module.exports = {
     show
 }
 
-function show(req, res) {
-    res.render('investments/show', {
-        investment: Investment.getOne(req.params.id),
-        title: 'Investment Details'
-    })
-}
 
 function index(req, res) {
     res.render('investments/index', {
         investments: Investment.getAll(),
         title: 'All Investments'
+    })
+}
+function show(req, res) {
+    res.render('investments/show', {
+        investment: Investment.getOne(req.params.id),
+        title: 'Investment Details'
     })
 }
 
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var methodOverride = require('method-override')
+// var methodOverride = require('method-override')
 
 var indexRouter = require('./routes/index');
 var investmentsRouter = require('./routes/investments');
@@ -18,13 +18,14 @@ app.use(function(req, res, next) {
   next()
 })
 
+// Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+// Routes
 app.use('/', indexRouter);
 app.use('/investments', investmentsRouter);
 
